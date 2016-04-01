a28f5554a332437f38092c6b581a5ab4d372142d
hanje1223 <jeehan@email.arizona.edu>
2016-04-01 15:31:44 -0700

    country

---
+++ country.R
@@ -0,0 +1,23 @@
+1 + 1
+gap.in <- read.csv("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv")
+gap.in
+head(gap.in
+attach(gap.in)
+subset(Thailand $country)
+
+selected <- subset(gap.in, country== "Vietnam", select = country)
+head(selected)
+selected <- subset(gap.in, country== "India", select = country)
+head(selected)
+selected <- subset(gap.in, country== "Venezuela, Vietnam, India", select = country)
+head(selected)
+selected
+c <- gap.in[which(gap.in$country=="vietnam"),]
+c
+head(c)c
+gap.in
+()
+c
+head(gap.in)
+c <- gap.in[which(gap.in$country=="Vietnam, Venezuela, India"),]
+head(c)
